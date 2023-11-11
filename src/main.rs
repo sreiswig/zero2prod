2d64f720c66ee791be4ab96bc351ecdcd77d2729
sreiswig <sam.reiswig@gmail.com>
2023-11-11 12:42:23 -0600

    Chapter 3.5 Random Port

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    run(listener)?.await
 }
