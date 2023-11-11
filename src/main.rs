865a54c1d68bc8983101792d00c575e3352c9687
sreiswig <sam.reiswig@gmail.com>
2023-11-11 12:47:01 -0600

    Chapter 3.5 random ports

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
